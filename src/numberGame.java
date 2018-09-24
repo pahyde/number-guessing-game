4f99d23c0aeac727838f42a876c0df3d58523ad0
Parker Hyde <parkerhyde79@gmail.com>
2018-09-24 17:22:25 -0400

    method refactor

--- src/numberGame.java
+++ src/numberGame.java
@@ -5,23 +5,28 @@ public class numberGame {
 	private static boolean userWantsToPlay = true;
 	private static int min = 1;
 	private static int max = 100;
-	private static int guesses = 0;
+	private static int games = 0;
+	private static int totalGuesses = 0;
+	private static int maxGuesses = 0;
     
 	public static void main(String[] args) {
+		
+		printIntroMessage();
+		
 		while (userWantsToPlay) {
 		    runGame();
-		    System.out.print("Do you want to keep playing? ");
+		    System.out.print("\nDo you want to play again? ");
 		    userWantsToPlay = Character.toLowerCase(scan.next().charAt(0)) == 'y';
-		    System.out.println();
-    	} 
+		}
+		displayGameStats();
     }
     
     public static void runGame() {
     	
-    	printIntroMessage();
-    	
-    	System.out.println("I'm thinking of a number...");
+    	System.out.println("\nI'm thinking of a number...");
     	int target = (int) (Math.random() * (max + 1 - min)) + min;
+    	
+    	int guesses = 0;
     	
     	while (true) {
     		System.out.print("Your guess? ");
@@ -33,21 +38,33 @@ public class numberGame {
     			System.out.println("lower");
     		} else {
     			System.out.println("You got it right in " + guesses + " guesses");
-    			guesses = 0;
+    			updateStats(guesses);
     			return;
     		}
     	}
     }
     
+    public static void updateStats(int guesses) {
+    	totalGuesses += guesses;
+    	maxGuesses = Math.max(maxGuesses, guesses);
+    	games++;
+    }
+    
+    public static void displayGameStats() {
+    	System.out.println("\nOveral results:");
+    	System.out.printf("%s\n%s\n%s\n%s\n",
+    			"    total games   = " + games,
+    			"    total guesses = " + totalGuesses,
+    			"    guesses/game  = " + (totalGuesses / games),
+    			"    max guesses   = " + maxGuesses);
+    }
+    
     public static void printIntroMessage() {
-    	String message = String.format("%s\n%s %d %s %d\n%s\n%s\n%s\n",
+    	System.out.printf("%s\n%s %d %s %d\n%s\n%s\n%s",
     			"This program allows you to play a guessing game.",
     			"I will think of a number between", min, "and", max,
     			"and will allow you to guess until you get it",
     			"For each guess, I will tell you whether the",
     			"right answer is higher or lower than your guess.");
-    	System.out.println(message);
     }
-    
-    
 }
